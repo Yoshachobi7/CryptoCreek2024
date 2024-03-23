ab071eb9d5ef9516fe3e312c42e87f29ae895e99
jharrell100161 <161148898+jharrell100161@users.noreply.github.com>
2024-03-23 11:55:07 -0400

    auto modes added

--- robot.py
+++ robot.py
@@ -25,12 +25,18 @@ class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         """Robot-wide initialization code should go here"""
 
+                                # Auto 0 : None
+        self.preferredAuto = 4  # Auto 1 : Drive forward
+                                # Auto 2 : Center speaker
+                                # Auto 3 : Side speaker
+                                # Auto 4 : Amp
+
         self.joystick = wpilib.XboxController(0)
 
-        self.lf_motor = rev.CANSparkMax(2, rev.CANSparkLowLevel.MotorType.kBrushed)
-        self.lr_motor = rev.CANSparkMax(9, rev.CANSparkLowLevel.MotorType.kBrushed)
-        self.rf_motor = rev.CANSparkMax(3, rev.CANSparkLowLevel.MotorType.kBrushed)
-        self.rr_motor = rev.CANSparkMax(4, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.lf_motor = rev.CANSparkMax(4, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.lr_motor = rev.CANSparkMax(3, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.rf_motor = rev.CANSparkMax(9, rev.CANSparkLowLevel.MotorType.kBrushed)
+        self.rr_motor = rev.CANSparkMax(2, rev.CANSparkLowLevel.MotorType.kBrushed)
 
         l_motor = wpilib.MotorControllerGroup(self.lf_motor, self.lr_motor)
         r_motor = wpilib.MotorControllerGroup(self.rf_motor, self.rr_motor)
@@ -48,10 +54,6 @@ class MyRobot(wpilib.TimedRobot):
         self.feed_motor.setInverted(True)
         self.launch_motor.setInverted(True)
 
-        # Apply current to launching mechanism
-        self.feed_motor.setSmartCurrentLimit(60)
-        self.launch_motor.setSmartCurrentLimit(60)
-
         # Inverting and applying current limit to roller claw and climber
         self.claw_motor.setInverted(False)
         self.climber_motor.setInverted(False)
@@ -61,27 +63,111 @@ class MyRobot(wpilib.TimedRobot):
 
         # Brake mode best for these motors and their use
         self.claw_motor.setIdleMode(rev.CANSparkBase.IdleMode.kBrake)
-                                                                                                       
-                                     # kBrake
         self.climber_motor.setIdleMode(rev.CANSparkBase.IdleMode.kBrake)   # kBrake
-
-        # Position gets automatically updated as robot moves
-        self.gyro = wpilib.AnalogGyro(1)
-
 
     def autonomousInit(self):
         """Called when autonomous mode is enabled"""
 
         self.timer = wpilib.Timer()
         self.timer.start()
+        self.lastAction = 0
 
     def autonomousPeriodic(self):
         """Movement during autonomous mode"""
-        #if self.timer.get() < 1.0:
-        #    self.drive.arcadeDrive(1.0, 1.0)
-        #else:
-         #    self.drive.arcadeDrive(0, 0)
         
+        # AUTO 0 #
+        if self.preferredAuto == 0:
+            self.drive.tankDrive(0,0)
+
+        # AUTO 1 #
+        if self.preferredAuto == 1:
+            # step 1: drive forward
+            if not self.timer.hasElapsed(2.5):
+                self.drive.tankDrive(-.5, -.5)
+            else:
+                self.drive.tankDrive(0, 0)
+
+        # AUTO 2 #
+        if self.preferredAuto == 2:
+            # step 1: launch
+            if not self.timer.hasElapsed(0.5):
+                self.launch_motor.set(1)
+            if self.timer.hasElapsed(0.5):
+                self.feed_motor.set(1)
+            if self.timer.hasElapsed(0.6):
+                self.launch_motor.set(0)
+                self.feed_motor.set(0)
+
+            # step 2: drive straight backwards
+            if self.timer.hasElapsed(0.7):
+                self.drive.tankDrive(0.5, 0.5)
+
+            if self.timer.hasElapsed(5):
+                self.drive.tankDrive(0,0)
+
+        # AUTO 3 #
+        if self.preferredAuto == 3:
+            # step 1: launch
+            if not self.timer.hasElapsed(1):
+                self.launch_motor.set(1)
+            if self.timer.hasElapsed(1):
+                self.lastAction = 1
+                self.feed_motor.set(1)
+            if self.timer.hasElapsed(self.lastAction + 0.1):
+                self.lastAction += 0.1
+                self.launch_motor.set(0)
+                self.feed_motor.set(0)
+
+            # step 2: drive straight backwards
+            if self.timer.hasElapsed(self.lastAction + 0.1):
+                self.lastAction += 0.1                
+                self.drive.tankDrive(0.5, 0.5)
+
+            # step 3: turn
+            if self.timer.hasElapsed(self.lastAction + 0.8):
+                self.lastAction += 0.8
+                self.drive.tankDrive(-0.5,0.5)
+
+            # step 4: continue driving
+            if self.timer.hasElapsed(self.lastAction + 0.25):
+                self.lastAction += 0.25
+                self.drive.tankDrive(0.5, 0.5)
+
+            if self.timer.hasElapsed(self.lastAction + 2):
+                self.lastAction += 2
+                self.drive.tankDrive(0, 0)
+
+        # AUTO 4 #
+        
+        if self.preferredAuto == 4:
+
+            # step 1: drive forward
+            if not self.timer.hasElapsed(0.385):
+                self.drive.tankDrive(-0.5, -0.5)
+            if self.timer.hasElapsed(0.385):
+                self.lastAction += 0.385
+                self.drive.tankDrive(0, 0)
+
+            # step 2: release claw note
+            if self.timer.hasElapsed(1):
+                self.lastAction += 0.7
+                self.claw_motor.set(-0.5)
+            if self.timer.hasElapsed(1.25):
+                self.lastAction += 0.25
+                self.claw_motor.set(0)
+
+            # step 3: turn
+            if self.timer.hasElapsed(1.5):
+                self.lastAction += 0.25
+                self.drive.tankDrive(-0.5,0.5)
+
+            # step 4: drive forwards
+            if self.timer.hasElapsed(2.5):
+                self.lastAction += 0.5
+                self.drive.tankDrive(-0.5, -0.5)
+            if self.timer.hasElapsed(4):
+                self.lastAction += 1.2
+                self.drive.tankDrive(0,0)
 
     def teleopPeriodic(self):
         """Called when operation control mode is enabled"""
@@ -101,8 +187,8 @@ class MyRobot(wpilib.TimedRobot):
             LeftY = (LeftY**4)*-1
         else:
             LeftY = (LeftY**4)
-        RightY = RightY *.5        
-        LeftY = LeftY *.5        
+        RightY = RightY *.9        
+        LeftY = LeftY *.9        
         #this makes it turn slower
         if((RightY < 0.1 and RightY > -0.1) and (LeftY >= 0.5 or LeftY <= -0.5)):
             LeftY = LeftY * 0.66
@@ -140,7 +226,7 @@ class MyRobot(wpilib.TimedRobot):
         # Spin the launcher motor for a second, then feed the note with feeder wheel
         if (self.joystick.getRawButton(kRB)):
             start = time.time()
-            while (time.time() < start + 1):
+            while (time.time() < start + .5):
                 self.launch_motor.set(1)
             self.feed_motor.set(1)
         elif (self.joystick.getRawButtonReleased(kRB)):
@@ -151,8 +237,8 @@ class MyRobot(wpilib.TimedRobot):
         # While amp button is being held, spin both motors to "spit" the note
         # out at a lower speed into the amp
         if (self.joystick.getRawButton(kX)):
-            self.launch_motor.set(.4)
-            self.feed_motor.set(.17)
+            self.launch_motor.set(.6)
+            self.feed_motor.set(.3)
         elif (self.joystick.getRawButtonReleased(kX)):
             self.launch_motor.set(0)
             self.feed_motor.set(0)
@@ -167,15 +253,15 @@ class MyRobot(wpilib.TimedRobot):
         else:
             self.claw_motor.set(0)
 
-        # CLIMBER MOTOR CONTROL
+        # CLIMBER MOTOR CONTR0OL
         # POV is D-Pad on controller, 0 == UP   180 == DOWN
-        if (self.joystick.getPOV() == 0):
-            self.climber_motor.set(1)
-        elif (self.joystick.getPOV() == 180):
+        if (self.joystick.getPOV() == 180):
             self.climber_motor.set(-1)
+        elif (self.joystick.getPOV() == 0):
+            self.climber_motor.set(1)
         else:
             self.climber_motor.set(0)
-
+        
     def disabledInit(self) -> None:
         # This just makes sure that our simulation code knows that the motor is off
         self.lf_motor.set(0)
